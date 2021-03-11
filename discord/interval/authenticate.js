a30d5893d45a55e42cdda4890b16886c5344c5a3
Twijn <twijn@twijn.net>
2021-03-11 16:22:52 -0600

    Add random colors, fix role management.

--- discord/interval/authenticate.js
+++ discord/interval/authenticate.js
@@ -1,6 +1,61 @@
 const con = require("../../database");
 const config = require("../../config.json");
 
+const colors = {
+    aqua: "#00ffff",
+    azure: "#f0ffff",
+    beige: "#f5f5dc",
+    black: "#000000",
+    blue: "#0000ff",
+    brown: "#a52a2a",
+    cyan: "#00ffff",
+    darkblue: "#00008b",
+    darkcyan: "#008b8b",
+    darkgrey: "#a9a9a9",
+    darkgreen: "#006400",
+    darkkhaki: "#bdb76b",
+    darkmagenta: "#8b008b",
+    darkolivegreen: "#556b2f",
+    darkorange: "#ff8c00",
+    darkorchid: "#9932cc",
+    darkred: "#8b0000",
+    darksalmon: "#e9967a",
+    darkviolet: "#9400d3",
+    fuchsia: "#ff00ff",
+    gold: "#ffd700",
+    green: "#008000",
+    indigo: "#4b0082",
+    khaki: "#f0e68c",
+    lightblue: "#add8e6",
+    lightcyan: "#e0ffff",
+    lightgreen: "#90ee90",
+    lightgrey: "#d3d3d3",
+    lightpink: "#ffb6c1",
+    lightyellow: "#ffffe0",
+    lime: "#00ff00",
+    magenta: "#ff00ff",
+    maroon: "#800000",
+    navy: "#000080",
+    olive: "#808000",
+    orange: "#ffa500",
+    pink: "#ffc0cb",
+    purple: "#800080",
+    violet: "#800080",
+    red: "#ff0000",
+    silver: "#c0c0c0",
+    white: "#ffffff",
+    yellow: "#ffff00"
+};
+
+const randomColor = () => {
+    let result;
+    let count = 0;
+    for (let prop in colors)
+        if (Math.random() < 1/++count)
+           result = colors[prop];
+    return result;
+}
+
 const https = require("https");
 const {MessageEmbed} = require("discord.js");
 
@@ -9,6 +64,7 @@ const FOLLOWER_REQUIREMENT = 2000;
 module.exports = client => {
 
     setInterval(() => {
+        try {
         con.query("select tm.id, tm.display_name, tm.discord_id from auth join twitchmod as tm on tm.id = auth.mod_id;", (err, res) => {
             if (err) {console.error(err);return;}
 
@@ -84,16 +140,19 @@ module.exports = client => {
 
                                         member.roles.remove(member.roles.cache).then(() => {
                                             let roles = []
+                                            let addedRoles = "";
                                             finalChannels.forEach(async channel => {
                                                 let role = guild.roles.cache.find(role => role.name.toLowerCase() === channel.toLowerCase());
 
                                                 if (role === null) {
                                                     message.guild.roles.create({
                                                         data: {
-                                                            name: channel.name.toLowerCase(),
+                                                            name: channel.toLowerCase(),
                                                             hoist: true,
-                                                            mentionable: true
-                                                        }
+                                                            mentionable: true,
+                                                            color: randomColor()
+                                                        },
+                                                        reason: "Role automatically added by ModBot",
                                                     }).then(newRole => {
                                                         roles = [
                                                             ...roles,
@@ -106,16 +165,16 @@ module.exports = client => {
                                                         role
                                                     ];
                                                 }
-
-                                                console.log(roles);
-
-                                                member.roles.add(roles);
-
-                                                const embed = new MessageEmbed()
-                                                        .setTitle("Account Linked!")
-                                                        .addField("Added Channels", "```" + channelStr + "```");
-                                                member.send(embed);
                                             });
+
+                                            console.log(roles);
+
+                                            member.roles.add(roles);
+
+                                            const embed = new MessageEmbed()
+                                                        .setTitle("Account Linked!")
+                                                        .addField("Added Channels", "```" + addedRoles + "```");
+                                            member.send(embed);
                                         });
                                     }
                                 } else {
@@ -129,6 +188,9 @@ module.exports = client => {
                 });
             });
         });
+        } catch (e) {
+            console.error(e);
+        }
     }, 1000);
 
 }
