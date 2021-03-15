cf6ee707651df1b36e4aeba2c8ead605e8287f80
Twijn <twijn@twijn.net>
2021-03-15 14:38:40 -0500

    Add Ludwig as disallowed channel

--- twitch/twitch.js
+++ twitch/twitch.js
@@ -10,7 +10,7 @@ const {MessageEmbed} = require("discord.js");
 let modSquadGuild = null;
 
 let channels = [];
-let disallowed_channels = ["@everyone", "modbot"];
+let disallowed_channels = ["@everyone", "modbot", "ludwig"];
 
 let bannedList = [];
 let timeoutList = [];
