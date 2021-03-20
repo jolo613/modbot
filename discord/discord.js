a20a0c9d739de942141829667c769cdbefefca65
Twijn <twijn@twijn.net>
2021-03-19 23:22:41 -0500

    Relay join messages in #who-you-mod-for

--- discord/discord.js
+++ discord/discord.js
@@ -51,7 +51,25 @@ client.on("guildMemberAdd", member => {
                 .setDescription(`Get access to TMS channels by authenticating your account with twitch [here](https://tmsqd.co/link/${member.id}).`)
                 .setColor(0x772ce8);
 
-        member.send(embed);
+        member.send(embed).then(() => {
+            if (config.hasOwnProperty("notification_channel")) {
+                const embedPublic = new Discord.MessageEmbed()
+                        .setTitle("Welcome to Twitch Mod Squad!")
+                        .setDescription("Follow the link sent in a DM to link your account to TMS. This will give you access to the rest of the channels!")
+                        .setColor(0x772ce8);
+    
+                member.guild.channels.resolve(config.notification_channel).send(embedPublic);
+            }
+        }).catch(() => {
+            if (config.hasOwnProperty("notification_channel")) {
+                const embedPublic = new Discord.MessageEmbed()
+                        .setTitle("Welcome to Twitch Mod Squad!")
+                        .setDescription("We weren't able to send you a DM! This is probably due to your privacy settings.\nTry sending !link directly to the ModBot user (me), change your privacy settings, or DM <@267380687345025025>")
+                        .setColor(0x772ce8);
+    
+                member.guild.channels.resolve(config.notification_channel).send(embedPublic);
+            }
+        });
     }
 });
 
