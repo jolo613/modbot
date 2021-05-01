4e41701c61b2c9ac1716a289d095d31e78f8c7b8
Twijn <twijn@twijn.net>
2021-05-01 12:46:17 -0500

    Fix indenting in database.js

--- database.js
+++ database.js
@@ -2,11 +2,11 @@ const config = require("./config.json");
 
 const mysql      = require('mysql');
 const connection = mysql.createConnection({
-  host     : config.mysql.host,
-  user     : config.mysql.user,
-  password : config.mysql.password,
-  database : config.mysql.database,
-  charset: 'utf8mb4',
+    host     : config.mysql.host,
+    user     : config.mysql.user,
+    password : config.mysql.password,
+    database : config.mysql.database,
+    charset: 'utf8mb4',
 });
  
 connection.connect();
