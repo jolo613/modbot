923a7ebc8afebca9dbbca482f55110c18949d997
Twijn <twijn@twijn.net>
2021-05-01 00:24:50 -0500

    Change limit from 250-> 100

--- discord/discord.js
+++ discord/discord.js
@@ -24,12 +24,12 @@ client.once('ready', () => {
 
     if (config.hasOwnProperty("liveban_channel")) {
         client.channels.fetch(config.liveban_channel).then(channel => {
-            con.query("select discord_message from ban where discord_message is not null order by timebanned desc limit 250, 1;", (err, afterBan) => {
+            con.query("select discord_message from ban where discord_message is not null order by timebanned desc limit 95, 1;", (err, afterBan) => {
                 if (err) {console.error(err);return;}
                 if (afterBan.length !== 1) {console.error("Query did not return 1 record");return;}
 
                 channel.messages.fetch({
-                    limit: 255,
+                    limit: 100,
                     after: afterBan[0].discord_message
                 }).then(messages => console.log(messages.size + " messages were successfully fetched from the #bans channel")).catch(console.error);
             });
