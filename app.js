7ce52f9167688634692b33282141e72a69d42921
Twijn <twijn@twijn.net>
2021-08-08 15:01:56 -0400

    add express backend to app.js

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+global.client = {};
+
 // Load Discord Module
 require("./discord/discord");
 
 // Load Twitch Module
 require("./twitch/twitch");
+
+// Load Express Backend
+require("./backend/express");
