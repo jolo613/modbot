9c6d01419aa9a392c6e99f46c0740f30e909c742
Twijn <twijn@twijn.net>
2021-08-13 12:47:27 -0400

    Big API changes

--- api.js
+++ api.js
@@ -10,7 +10,7 @@ const api = new ApiClient({ authProvider });
 class IdentityService {
     resolveIdentity(identityId) {
         return new Promise((resolve, reject) => {
-            let identity = {};
+            let identity = {avatar_url: "/assets/images/bop.png"};
             con.query("select id, name from identity where id = ?;", [identityId], (err, res) => {
                 if (err) {reject(err);return;}
 
@@ -24,8 +24,24 @@ class IdentityService {
                     tus.resolveIdentityProfiles(identityId).then(twitchProfiles => {
                         identity.profiles = {twitch: twitchProfiles};
 
+                        if (twitchProfiles.length > 0) {
+                            twitchProfiles.forEach(profile => {
+                                if (profile.profile_image_url !== null && profile.profile_image_url !== "") {
+                                    identity.avatar_url = profile.profile_image_url;
+                                }
+                            });
+                        }
+
                         dus.resolveIdentityProfiles(identityId).then(discordProfiles => {
                             identity.profiles.discord = discordProfiles;
+
+                            if (discordProfiles.length > 0) {
+                                discordProfiles.forEach(profile => {
+                                    if (profile.avatar !== null && profile.avatar !== "") {
+                                        identity.avatar_url = `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.png`;
+                                    }
+                                });
+                            }
 
                             resolve(identity);
                         })
@@ -33,6 +49,36 @@ class IdentityService {
                 } else {
                     reject("Identity not found");
                 }
+            });
+        });
+    }
+
+    linkModerator(moderatorId, streamerId) {
+        return new Promise((resolve, reject) => {
+            con.query("insert into identity__moderator (identity_id, modfor_id) values (?, ?);", [moderatorId, streamerId], err => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
+            });
+        });
+    }
+
+    getStreamers(moderatorId) {
+        return new Promise((resolve, reject) => {
+            con.query("select identity.id from identity__moderator join identity on identity.id = identity__moderator.modfor_id where identity__moderator.identity_id = ?;", [moderatorId], async (err, res) => {
+                if (err) {reject(err);return;}
+                let result = [];
+
+                for (var i = 0; i < res.length; i++) {
+                    result = [
+                        ...result,
+                        await this.resolveIdentity(res[i].id),
+                    ];
+                }
+
+                resolve(result);
             });
         });
     }
@@ -52,34 +98,93 @@ class TwitchUserService {
             affiliation: result.affiliation,
             identity: {
                 id: result.identity_id,
-                name: result.identity_name
+                name: result.identity_name,
             },
         };
     }
 
     resolveByName(userName) {
         return new Promise((resolve, reject) => {
-            con.pquery("select id, display_name, identity_id, identity.name as identity_name, email, profile_image_url, offline_image_url, description, view_count, follower_count, affiliation from twitch__user join identity on identity.id = twitch__user.identity_id where display_name = ?;", [userName]).then(result => {
+            con.pquery("select twitch__user.id, display_name, identity_id, identity.name as identity_name, email, profile_image_url, offline_image_url, description, view_count, follower_count, affiliation from twitch__user left join identity on identity.id = twitch__user.identity_id where display_name = ?;", [userName]).then(async result => {
                 if (result.length > 0) {
                     let user = result[0];
                     resolve(this.#resolveUserObj(user));
                 } else {
+                    let helixUser = await api.helix.users.getUserByName(userName);
 
+                    if (helixUser) {
+                        con.query("insert into twitch__user (id, display_name, identity_id, email, profile_image_url, offline_image_url, description, view_count, follower_count, affiliation) values (?, ?, null, null, ?, ?, ?, ?, null, ?);", [
+                            helixUser.id,
+                            helixUser.displayName,
+                            helixUser.profilePictureUrl,
+                            helixUser.offlinePlaceholderUrl,
+                            helixUser.description,
+                            helixUser.views,
+                            helixUser.broadcasterType,
+                        ]);
+
+                        resolve({
+                            id: helixUser.id,
+                            display_name: helixUser.displayName,
+                            profile_image_url: helixUser.profilePictureUrl,
+                            offline_image_url: helixUser.offlinePlaceholderUrl,
+                            description: helixUser.description,
+                            view_count: helixUser.views,
+                            follower_count: null,
+                            affiliation: helixUser.broadcasterType,
+                            identity: {
+                                id: null,
+                                name: null,
+                            },
+                        });
+                    } else {
+                        reject("User not found");
+                    }
                 }
             }).catch(reject);
         });
     }
 
-    resolveById(userId) {
+    resolveById(userId, displayName = null) {
         return new Promise((resolve, reject) => {
-            con.pquery("select id, display_name, identity_id, identity.name as identity_name, email, profile_image_url, offline_image_url, description, view_count, follower_count, affiliation from twitch__user join identity on identity.id = twitch__user.identity_id where id = ?;", [userId]).then(result => {
+            con.pquery("select twitch__user.id, display_name, identity_id, identity.name as identity_name, email, profile_image_url, offline_image_url, description, view_count, follower_count, affiliation from twitch__user left join identity on identity.id = twitch__user.identity_id where twitch__user.id = ?;", [userId]).then(result => {
                 if (result.length > 0) {
                     let user = result[0];
                     resolve(this.#resolveUserObj(user));
-                } else {
+                } else if (displayName !== null) {
+                    con.query("insert into twitch__user (id, display_name) values (?, ?);", [userId, displayName], err => {
+                        if (err) {reject(err);return;}
 
+                        this.resolveById(userId).then(resolve).catch(reject);
+                    });
+                } else {
+                    reject("Could not retrieve user");
                 }
             }).catch(reject);
+        });
+    }
+
+    resolveIdentity(twitchId, twitchName) {
+        return new Promise((resolve, reject) => {
+            con.query("select identity.id, identity.name from twitch__user as tu left join identity on identity.id = tu.identity_id where tu.id = ?;", twitchId, (err, res) => {
+                if (err) {reject(err);return;}
+
+                if (res.length > 0) {
+                    if (res[0].id !== null) {
+                        resolve(res[0]);
+                    } else {
+                        con.query("insert into identity (name) values (?);", [twitchName], (err, res) => {
+                            if (err) {reject(err);return;}
+
+                            let id = res.insertId;
+                            con.query("update twitch__user set identity_id = ? where id = ?;", [id, twitchId]);
+                            resolve({id: id, name: twitchName});
+                        });
+                    }
+                } else {
+                    reject("No record found.")
+                }
+            });
         });
     }
 
