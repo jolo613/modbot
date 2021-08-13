61cecca118b9a07e0305fb907999edb8ab361da8
Twijn <twijn@twijn.net>
2021-08-13 12:47:42 -0400

    Add websocket and fetchModerators interval to app.js

--- app.js
+++ app.js
@@ -9,9 +9,15 @@ require("./twitch/twitch");
 // Load Express Backend
 require("./backend/express");
 
+// Load websocket
+require("./websocket/websocket");
+
 // Load global intervals
 const updateUsers = require("./interval/updateUsers");
+const fetchModerators = require("./interval/fetchModerators");
 
 updateUsers();
+fetchModerators();
 
 setInterval(updateUsers, 30000);
+setInterval(fetchModerators, 30000);
