60781edafbe43e4fc359098dcd71d631956d09d6
Twijn <twijn@twijn.net>
2021-08-14 12:02:52 -0400

    Add updating of discord usernames

--- app.js
+++ app.js
@@ -16,11 +16,14 @@ require("./websocket/websocket");
 const updateUsers = require("./interval/updateUsers");
 const fetchModerators = require("./interval/fetchModerators");
 const updateTwitchUsernames = require("./interval/updateTwitchUsernames");
+const updateDiscordUsernames = require("./interval/updateDiscordUsernames");
 
 updateUsers();
 fetchModerators();
 updateTwitchUsernames();
+updateDiscordUsernames();
 
 setInterval(updateUsers, 30000);
 setInterval(fetchModerators, 30000);
 setInterval(updateTwitchUsernames, 60000);
+setInterval(updateDiscordUsernames, 60000);
---
+++ interval/updateDiscordUsernames.js
@@ -0,0 +1,16 @@
+const con = require("../database");
+const API = require("../api");
+
+module.exports = () => {
+    con.query("select discord__user.id, discord__user.name, discord__user.discriminator from discord__user left join discord__username on discord__user.id = discord__username.id and discord__user.name = discord__username.name and discord__user.discriminator = discord__username.discriminator where discord__username.id is null;", (err, res) => {
+        if (err) {console.error(err);return;}
+
+        res.forEach(user => {
+            con.query("update discord__username set last_seen = now() where id = ? and last_seen is null;", [user.id], err => {
+                if (err) console.error(err);
+
+                con.query("insert into discord__username (id, name, discriminator) values (?, ?, ?);", [user.id, user.name, user.discriminator]);
+            });
+        })
+    });
+};
