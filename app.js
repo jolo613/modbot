e2430c367f37fdebbc31d31cfb38e05e4b89e426
Twijn <twijn@twijn.net>
2021-08-14 11:47:30 -0400

    Add updating of twitch__username

--- app.js
+++ app.js
@@ -15,9 +15,12 @@ require("./websocket/websocket");
 // Load global intervals
 const updateUsers = require("./interval/updateUsers");
 const fetchModerators = require("./interval/fetchModerators");
+const updateTwitchUsernames = require("./interval/updateTwitchUsernames");
 
 updateUsers();
 fetchModerators();
+updateTwitchUsernames();
 
 setInterval(updateUsers, 30000);
 setInterval(fetchModerators, 30000);
+setInterval(updateTwitchUsernames, 60000);
---
+++ interval/updateTwitchUsernames.js
@@ -0,0 +1,16 @@
+const con = require("../database");
+const API = require("../api");
+
+module.exports = () => {
+    con.query("select twitch__user.id, twitch__user.display_name, twitch__user.last_updated from twitch__user left join twitch__username on twitch__username.id = twitch__user.id where twitch__username.id is null;", (err, res) => {
+        if (err) {console.error(err);return;}
+
+        res.forEach(user => {
+            con.query("update twitch__username set last_seen = now() where id = ?;", [user.id], err => {
+                if (err) console.error(err);
+
+                con.query("insert into twitch__username (id, display_name, first_seen) values (?, ?, ?);", [user.id, user.display_name, user.last_updated]);
+            });
+        })
+    });
+};
