0f3b6fece272405f1c09ba129f837feba8255061
Twijn <twijn@twijn.net>
2021-09-11 15:22:22 +0000

    Implement caching for most API functions

--- api.js
+++ api.js
@@ -7,9 +7,56 @@ const {ClientCredentialsAuthProvider} = require("twitch-auth");
 const authProvider = new ClientCredentialsAuthProvider(config.twitch.client_id, config.twitch.client_secret);
 const api = new ApiClient({ authProvider });
 
+let services = {};
+
+class Cache {
+    cache = {};
+
+    constructor(timeout = 900) { // timeout in seconds
+        this.timeout = timeout;
+
+        setInterval(() => {
+            let curTime = new Date().getTime();
+            let res = {};
+            for (let key in this.cache) {
+                if (curTime <= this.cache[key].cu) {
+                    res[key] = this.cache[key];
+                }
+            }
+        }, Math.floor(Math.max(1, timeout / 50) * 1000));
+    };
+
+    get = async (key, retrieve, overrideCache = false, attemptInt = true) => {
+        if (attemptInt && typeof(key) !== "number") {
+            try {
+                let pikey = Integer.parseInt(key);
+                key = pikey;
+            } catch(e) {}
+        }
+        if (!overrideCache && this.cache.hasOwnProperty(key)) {
+            return this.cache[key];
+        } else {
+            let item = await new Promise(retrieve);
+            this.put(key, item);
+            return item;
+        }
+    };
+
+    put = (key, item) => {
+        item.cu = new Date().getTime() + (this.timeout * 1000);
+        this.cache[key] = item;
+    }
+
+    expire = key => {
+        delete this.cache[key];
+    }
+}
+
 class SessionService {
+    cache = new Cache();
+
     resolveSession(sessionId) {
-        return new Promise((resolve, reject) => {
+        return this.cache.get(sessionId, (resolve, reject) => {
             con.query("select id, identity_id, created from session where id = ?;", [sessionId], (err, res) => {
                 if (err) {reject(err);return;}
 
@@ -24,15 +71,17 @@ class SessionService {
 }
 
 class IdentityService {
-    resolveIdentity(identityId, includeStreamers = true) {
-        return new Promise((resolve, reject) => {
+    cache = new Cache();
+
+    resolveIdentity(identityId, includeStreamers = true, overrideCache = false) {
+        return this.cache.get(identityId, (resolve, reject) => {
             let identity = {avatar_url: "https://p.tmsqd.co/assets/images/bop.png"};
             con.query("select id, name from identity where id = ?;", [identityId], (err, res) => {
                 if (err) {reject(err);return;}
 
                 if (res.length > 0) {
-                    const tus = new TwitchUserService();
-                    const dus = new DiscordUserService();
+                    const tus = services.TwitchUserService;
+                    const dus = services.DiscordUserService;
 
                     identity.id = res[0].id;
                     identity.name = res[0].name;
@@ -74,7 +123,7 @@ class IdentityService {
                     reject("Identity not found");
                 }
             });
-        });
+        }, overrideCache);
     }
 
     unlinkModerators(moderatorId) {
@@ -89,12 +138,12 @@ class IdentityService {
         });
     }
 
-    linkModerator(moderatorId, streamerId) {
+    linkModerator(moderatorId, streamerId, active = true) {
         return new Promise((resolve, reject) => {
             con.query("select * from identity__moderator where identity_id = ? and modfor_id = ?;", [moderatorId, streamerId], (err, res) => {
                 if (!err) {
                     if (res.length === 0) {
-                        con.query("insert into identity__moderator (identity_id, modfor_id) values (?, ?);", [moderatorId, streamerId], err => {
+                        con.query("insert into identity__moderator (identity_id, modfor_id, active) values (?, ?, ?);", [moderatorId, streamerId, active], err => {
                             if (err) {
                                 reject(err);
                             } else {
@@ -113,14 +162,16 @@ class IdentityService {
 
     getStreamers(moderatorId) {
         return new Promise((resolve, reject) => {
-            con.query("select identity.id from identity__moderator join identity on identity.id = identity__moderator.modfor_id where identity__moderator.identity_id = ?;", [moderatorId], async (err, res) => {
+            con.query("select identity.id, identity__moderator.active from identity__moderator join identity on identity.id = identity__moderator.modfor_id where identity__moderator.identity_id = ?;", [moderatorId], async (err, res) => {
                 if (err) {reject(err);return;}
                 let result = [];
 
                 for (var i = 0; i < res.length; i++) {
+                    let response = await this.resolveIdentity(res[i].id, false);
+                    response.active = res[i].active;
                     result = [
                         ...result,
-                        await this.resolveIdentity(res[i].id, false),
+                        response,
                     ];
                 }
 
@@ -159,6 +210,7 @@ class IdentityService {
 }
 
 class TwitchUserService {
+    cache = new Cache();
 
     #resolveUserObj(result) {
         return {
@@ -219,12 +271,12 @@ class TwitchUserService {
         });
     }
 
-    resolveById(userId, displayName = null) {
-        return new Promise((resolve, reject) => {
+    resolveById(userId, displayName = null, overrideCache = false) {
+        return this.cache.get(userId, (resolve, reject) => {
             con.pquery("select twitch__user.id, display_name, identity_id, identity.name as identity_name, email, profile_image_url, offline_image_url, description, view_count, follower_count, affiliation from twitch__user left join identity on identity.id = twitch__user.identity_id where twitch__user.id = ?;", [userId]).then(result => {
                 if (result.length > 0) {
-                    let user = result[0];
-                    resolve(this.#resolveUserObj(user));
+                    let user = this.#resolveUserObj(result[0]);
+                    resolve(user);
                 } else if (displayName !== null) {
                     con.query("insert into twitch__user (id, display_name) values (?, ?);", [userId, displayName], err => {
                         if (err) {reject(err);return;}
@@ -235,7 +287,7 @@ class TwitchUserService {
                     reject("Could not retrieve user");
                 }
             }).catch(reject);
-        });
+        }, overrideCache);
     }
 
     resolveIdentity(twitchId, twitchName) {
@@ -280,7 +332,7 @@ class TwitchUserService {
                 let results = [];
 
                 for (let i = 0; i < res.length; i++) {
-                    let identity = await (new IdentityService()).resolveIdentity(res[i].identity_id);
+                    let identity = await services.IdentityService.resolveIdentity(res[i].identity_id);
                     identity.last_active = res[i].last_active;
 
                     results = [
@@ -297,6 +349,9 @@ class TwitchUserService {
 }
 
 class DiscordUserService {
+    cache = new Cache();
+    nameCache = new Cache(1800);
+
     resolveIdentityProfiles(identityId) {
         return new Promise((resolve, reject) => {
             con.query("select id, name, discriminator, avatar, identity_id from discord__user where identity_id = ?;", identityId, (err, res) => {
@@ -312,7 +367,7 @@ class DiscordUserService {
             con.query("select identity_id from discord__user where id = ?;", [discordId], (err, res) => {
                 if (err) {reject(err);return;}
 
-                let ids = new IdentityService();
+                let ids = services.IdentityService;
                 if (res.length > 0) {
                     ids.resolveIdentity(res[0].identity_id).then(resolve, reject);
                 } else {
@@ -327,6 +382,26 @@ class DiscordUserService {
                     });
                 }
             });
+        });
+    }
+
+    resolveById(userId) {
+        return this.cache.get(userId, (resolve, reject) => {
+            con.query("select * from discord__user where id = ?;", [userId], (err, res) => {
+                if (err) {reject(err);return;}
+
+                if (res.length > 0) {
+                    resolve(res[0]);
+                } else {
+                    reject("User not found");
+                }
+            });
+        });
+    }
+
+    resolveByName(name, discriminator = null) {
+        return this.nameCache.get(name + "#" + discriminator, (resolve, reject) => {
+            con.query("select user.* from discord__username as un join discord__user as user on user.id = un.id;");
         });
     }
 
@@ -397,10 +472,12 @@ class DiscordUserService {
     }
 }
 
-module.exports = {
+services = {
     SessionService: new SessionService(),
     IdentityService: new IdentityService(),
     TwitchUserService: new TwitchUserService(),
     DiscordUserService: new DiscordUserService(),
     TwitchAPI: api,
 };
+
+module.exports = services;
