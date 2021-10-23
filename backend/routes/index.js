79e893d439de77651c919cb80218a7096c988174
Twijn <twijn@twijn.net>
2021-10-23 21:13:12 +0000

    Complete /auth/ route addition

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,11 @@
 const identity = require("./identity");
 const streamers = require("./streamers");
 const status = require("./status");
+const auth = require("./auth");
  
 module.exports = {
     identity,
     streamers,
     status,
+    auth,
 };
