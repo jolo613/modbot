019603a8a3231a3378e80167b9af28b6502c5420
Twijn <twijn@twijn.net>
2021-12-28 16:13:08 +0000

    Update how updateUsers works, where it will request multiple users at the same time, where possible.

--- interval/updateUsers.js
+++ interval/updateUsers.js
@@ -1,35 +1,61 @@
 const con = require("../database");
 const {TwitchAPI} = require("../api");
+const {Twitch} = require("../api/index");
+
+let waiting = false;
 
 module.exports = () => {
-    con.query("select id, display_name from twitch__user where follower_count is null or date_add(last_updated, interval 7 day) < now() limit 2;", (err, res) => {
+    if (waiting) return;
+    waiting = true;
+    con.query("select id, display_name from twitch__user where follower_count is null or date_add(last_updated, interval 7 day) < now() limit 100;", async (err, res) => {
         if (err) return;
-        
-        res.forEach(async user => {
-            let followers = await TwitchAPI.helix.users.getFollows({followedUser: user.id});
 
-            con.query("update twitch__user set follower_count = ? where id = ?;", [followers.total, user.id]);
+        let userIds = [];
 
-            let helixUser = await TwitchAPI.helix.users.getUserById(user.id);
+        res.forEach(user => {
+            userIds = [
+                ...userIds,
+                user.id,
+            ]
+        });
 
-            if (helixUser === null) {
-                con.query("update twitch__user set last_updated = date_add(now(), interval 2 day) where id = ?;", [user.id]);
-                return;
-            }
+        if (userIds.length < 10) return;
+
+        console.log("Sending request...");
+
+        let startTime = new Date().getTime();
+
+        let helixUsers = await TwitchAPI.helix.users.getUsersByIds(userIds);
+
+        console.log(`Received ${helixUsers.length}/${userIds.length} users: ${new Date().getTime() - startTime} ms`);
+        waiting = false;
+
+        helixUsers.forEach(async helixUser => {
+            let user = await Twitch.getUserById(helixUser.id);
 
             if (helixUser.displayName.toLowerCase() !== user.display_name.toLowerCase()) {
                 con.query("update twitch__username set last_seen = now() where id = ? and display_name = ?;", [user.id, user.display_name]);
                 con.query("insert into twitch__username (id, display_name) values (?, ?);", [user.id, helixUser.displayName]);
             }
 
-            con.query("update twitch__user set display_name = ?, description = ?, profile_image_url = ?, offline_image_url = ?, view_count = ?, last_updated = now() where id = ?;", [
+            con.query("update twitch__user set display_name = ?, description = ?, profile_image_url = ?, offline_image_url = ?, view_count = ?, affiliation = ?, last_updated = now() where id = ?;", [
                 helixUser.displayName,
                 helixUser.description,
                 helixUser.profilePictureUrl,
                 helixUser.offlinePlaceholderUrl,
                 helixUser.views,
                 helixUser.id,
+                helixUser.broadcasterType ? null : helixUser.broadcasterType,
             ]);
+        });
+
+        con.query("update twitch__user set last_updated = now() where id in (" + (", ?").repeat(userIds.length).substring(2) + ");", userIds);
+        
+        userIds.forEach(async userId => {
+
+            let followers = await TwitchAPI.helix.users.getFollows({followedUser: userId});
+
+            con.query("update twitch__user set follower_count = ? where id = ?;", [followers.total, userId]);
         });
     });
 };
