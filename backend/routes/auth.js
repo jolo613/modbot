4059d75c425d82fa49102e98f40498316bf182a0
Twijn <twijn@twijn.net>
2021-12-28 16:12:33 +0000

    Updates to api/auth/x.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,17 +1,28 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const {Router} = require("express");
+const con = require("../../database");
 const {TwitchUserService, BackendAPI} = require("../../api");
+
+const api = require("../../api/index");
+const Session = require("../../api/Session");
 
 const config = require("../../config.json");
 
 const https = require("https");
+const FullIdentity = require('../../api/FullIdentity');
+const TwitchUser = require('../../api/Twitch/TwitchUser');
+const DiscordUser = require('../../api/Discord/DiscordUser');
  
 const router = Router();
+
+const PANEL_URL = "https://p.tmsqd.co";
 
 const TWITCH_URL = "https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=qsedbwr82672tfg7fvobxlf01ljoov&redirect_uri=https%3A%2F%2Fapi.tmsqd.co%2Fauth%2Ftwitch&scope=user%3Aread%3Aemail";
 const TWITCH_REDIRECT = "https://api.tmsqd.co/auth/twitch";
 const DISCORD_URL = "https://discord.com/api/oauth2/authorize?client_id=819821932253937686&redirect_uri=https%3A%2F%2Fapi.tmsqd.co%2Fauth%2Fdiscord&response_type=code&scope=guilds.join%20identify";
 const DISCORD_REDIRECT = "https://api.tmsqd.co/auth/discord";
+
+const FOLLOWER_REQUIREMENT = 5000;
 
 const discord = {
     async getToken(code) {
@@ -59,13 +70,6 @@ const twitch = {
         return await oauthResult.json();
     },
     async getUser(tokenType, accessToken) {
-        console.log({
-            method: 'GET',
-            headers: {
-                ["Client-ID"]: config.twitch.client_id,
-                Authorization: `Bearer ${accessToken}`,
-            },
-        });
         const userResult = await fetch('https://api.twitch.tv/helix/users', {
             method: 'GET',
             headers: {
@@ -75,7 +79,7 @@ const twitch = {
         });
         return await userResult.json();
     },
-    getModerators(user) {
+    linkModerators(user) {
         return new Promise((res, rej) => {
             https.request({
                 host: "modlookup.3v.fi",
@@ -91,8 +95,6 @@ const twitch = {
                     try {
                         let data = JSON.parse(str.trim());
     
-                        console.log(data);
-    
                         if (data.status == 200) {
                             if (data.hasOwnProperty("channels") && data.channels.length > 0) {
     
@@ -102,9 +104,6 @@ const twitch = {
                                     try {
                                         let streamer = await TwitchUserService.resolveByName(channel.name);
                                         let streamerIdentity = await TwitchUserService.resolveIdentity(streamer.id, streamer.display_name);
-    
-                                        if (channel.followers >= FOLLOWER_REQUIREMENT)
-                                            twitch.listenOnChannel(channel.name.toLowerCase());
                                         
                                         await IdentityService.linkModerator(identity.id, streamerIdentity.id, channel.followers >= FOLLOWER_REQUIREMENT);
                                     } catch (e) {
@@ -113,21 +112,8 @@ const twitch = {
                                         }
                                     }
                                 }
-    
-                                con.query("update twitch__user set moderator_checked = now() where id = ?;",[user.id], async () => {
-                                    global.websocket.emit({identityId: identity.id}, {type: "streamer-list-updated", data: await IdentityService.getStreamers(identity.id)});
-                                    identity = await IdentityService.resolveIdentity(identity.id, undefined, true);
-    
-                                    updateRoles(identity);
-    
-                                    if (user.moderator_checked === null) {
-                                        sendUpdate(identity);
-                                    }
-                                });
                             } else {
-                                con.query("update twitch__user set moderator_checked = now() where id = ?;", [user.id], () => {
-                                    global.websocket.emit({identityId: identity.id}, {type: "no-action-notification", data: {title: "Moderation Check Update", description: "Twitch user " + data.user + " was linked, but isn't listed as a moderator anywhere!"}});
-                                });
+                                rej("User was not listed as a moderator anywhere!");
                             }
                         } 
                     } catch (e) {
@@ -139,7 +125,26 @@ const twitch = {
     }
 }
 
-router.get("/twitch", async ({query}, res) => {
+router.use("/", (req, res, next) => {
+    const { invite } = req.cookies;
+
+    if (invite) {
+        con.query("select initiated_by from invite where invite = ?;", [invite], (err, res) => {
+            if (err) console.error(err);
+            
+            if (res.length > 0) {
+                req.invitee = res[0].initiated_by;
+                next();
+            } else {
+                res.json({success: false, error: "Invalid Invite Link"});
+            }
+        });
+    } else {
+        res.json({success: false, error: "Invalid Invite Link"});
+    }
+});
+
+router.get("/twitch", async ({ query, cookies }, res) => {
     const { code } = query;
 
     if (code) {
@@ -149,21 +154,82 @@ router.get("/twitch", async ({query}, res) => {
             res.redirect(TWITCH_URL);
         }
 
-        console.log(oauthData);
+        const result = await twitch.getUser(oauthData.token_type, oauthData.access_token);
 
-        const user = await twitch.getUser(oauthData.token_type, oauthData.access_token);
-        res.json(user);
+        if (result.data?.hasOwnProperty(0)) {
+            const user = result.data[0];
+            api.Twitch.getUserById(user.id, true).then(async twitchUser => {
+                let session;
+
+                if (cookies.session) {
+                    try {
+                        session = await api.getSession(cookies.session);
+                    } catch (err) {}
+                }
+
+                // catch all, if a session isn't present, make one.
+                if (session === undefined)
+                    session = new Session(api.stringGenerator(32), null, null);
+
+
+                // attempt to load the identity
+                if (twitchUser.identity?.id !== null) {
+                    try {
+                        session.identity = await api.getFullIdentity(twitchUser.identity.id);
+                    } catch(err) {}
+                }
+
+                twitchUser.display_name = user.display_name;
+                twitchUser.email = user.email;
+                twitchUser.profile_image_url = user.profile_image_url;
+                twitchUser.offline_image_url = user.offline_image_url;
+                twitchUser.description = user.description;
+                twitchUser.view_count = user.view_count;
+                twitchUser.affiliation = user.affiliation;
+
+                // catch all, if an identity isn't present, make one.
+                if (!session.identity) {
+                    session.identity = new FullIdentity(null, user.display_name, [twitchUser], null);
+                } else if (!session.identity.twitchAccounts.find(x => x.id === twitchUser.id)) {
+                    session.identity.twitchAccounts = [
+                        ...session.identity.twitchAccounts,
+                        twitchUser,
+                    ];
+                }
+
+                // post that sh!t
+                session = await session.post();
+
+                res.cookie("session", session.id, {domain: ".tmsqd.co", maxAge: new Date(Date.now() + 86400000), path: "/", secure: true});
+                
+                if (session.identity?.discordAccounts?.length > 0) {
+                    res.redirect(PANEL_URL);
+                } else {
+                    res.redirect(DISCORD_URL);
+                }
+            }, err => {
+                console.log(err);
+            });
+        }
     } else {
         res.redirect(TWITCH_URL);
     }
 });
 
-router.get('/discord', async ({ query, cookies }, res) => {
+router.get('/discord', async ({ query, cookies, invitee }, res) => {
 	const { code } = query;
-    const { session } = cookies;
 
-    if (!session) {
-        BackendAPI.createSession();
+    let session = undefined;
+
+    if (cookies?.session) {
+        try {
+            session = await api.getSession(cookies.session);
+        } catch (err) {}
+    }
+    
+    if (session === undefined) {
+        res.redirect(TWITCH_URL);
+        return;
     }
 
 	if (code) {
@@ -175,7 +241,101 @@ router.get('/discord', async ({ query, cookies }, res) => {
                 res.redirect(DISCORD_URL);
                 return;
             }
-            res.json(user);
+            
+            let dus = new DiscordUser(
+                    user.id,
+                    null,
+                    user.username,
+                    user.discriminator,
+                    user.avatar
+                );
+
+            if (session.identity.discordAccounts.length === 0) {
+                con.query("insert into invite__uses (invited, invitee) values (?,?) on duplicate key update invitee = ?;", [session.identity.id, invitee, invitee]);
+            }
+
+            if (!session.identity.discordAccounts.find(x => x.id === dus.id)) {
+                session.identity.discordAccounts = [...session.identity.discordAccounts, dus];
+            }
+
+            session = await session.post();
+
+            let partnered = false;
+            let affiliate = false;
+            let partneredModerator = false;
+            let affiliateModerator = false;
+
+            console.log(session.identity.twitchAccounts);
+
+            for (let ri = 0; ri < session.identity.twitchAccounts.length; ri++) {
+                let twitchAccount = session.identity.twitchAccounts[ri];
+
+                let followers = await twitchAccount.refreshFollowers();
+
+                if (twitchAccount.affiliation === "partner") {
+                    partnered = true;
+                } else if (twitchAccount.affiliation === "affiliate" && followers >= FOLLOWER_REQUIREMENT) {
+                    affiliate = true;
+                }
+
+                let streamers = await twitchAccount.refreshStreamers();
+
+                for (let si = 0; si < streamers.length; si++) {
+                    let streamer = streamers[si];
+
+                    followers = await streamer.refreshFollowers();
+                    
+                    if (streamer.affiliation === "partner") {
+                        partneredModerator = true;
+                    } else if (streamer.affiliation === "affiliate" && followers >= FOLLOWER_REQUIREMENT) {
+                        affiliateModerator = true;
+                    }
+                }
+            }
+
+            let resolvedRoles = [];
+
+            if (partnered) {
+                resolvedRoles = [
+                    ...resolvedRoles,
+                    config.partnered.streamer
+                ];
+            }
+            if (affiliate) {
+                resolvedRoles = [
+                    ...resolvedRoles,
+                    config.affiliate.streamer
+                ];
+            }
+            if (partneredModerator) {
+                resolvedRoles = [
+                    ...resolvedRoles,
+                    config.partnered.moderator
+                ];
+            }
+            if (affiliateModerator) {
+                resolvedRoles = [
+                    ...resolvedRoles,
+                    config.affiliate.moderator
+                ];
+            }
+
+            if (resolvedRoles.length > 0) {
+                global.client.discord.guilds.fetch(config.modsquad_discord).then(guild => {
+                    guild.members.add(dus.id, {accessToken: oauthData.access_token, roles: resolvedRoles}).then(member => {
+                        res.redirect(PANEL_URL);
+                    }).catch((err) => {
+                        console.error(err);
+                        res.json({success: false, error: "Could not add user to Discord"});
+                    });
+                }).catch((err) => {
+                    console.error(err);
+                    res.json({success: false, error: "Could not obtain guild"});
+                });
+            } else {
+                res.status(401)
+                res.json({success: false, error: "Did not meet join criteria. If you believe this is in error, send this page to @Twijn#8888 on Discord.", session: session});
+            }
 		} catch (error) {
 			// NOTE: An unauthorized token will not throw an error;
 			// it will return a 401 Unauthorized response in the try block above
