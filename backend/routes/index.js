27b2f2aed65c795b90cd4e69849239a525a524f5
Twijn <twijn@twijn.net>
2022-01-14 14:33:27 +0000

    Add contact us and search routes to routes index

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,10 +2,14 @@ const identity = require("./identity");
 const streamers = require("./streamers");
 const status = require("./status");
 const auth = require("./auth");
+const search = require("./search");
+const contactUs = require("./contact-us");
  
 module.exports = {
     identity,
     streamers,
     status,
     auth,
+    search,
+    contactUs,
 };
