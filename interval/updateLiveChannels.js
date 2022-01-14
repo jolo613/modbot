894129333555c5d81f76ad7a3a5c920280bdcaa6
Twijn <twijn@twijn.net>
2022-01-14 14:33:47 +0000

    Add live channel feature

---
+++ interval/updateLiveChannels.js
@@ -0,0 +1,129 @@
+const con = require("../database");
+const config = require("../config.json");
+
+const {MessageEmbed} = require("discord.js");
+
+const {ApiClient} = require("twitch");
+const {ClientCredentialsAuthProvider} = require("twitch-auth");
+
+const authProvider = new ClientCredentialsAuthProvider(config.twitch.client_id, config.twitch.client_secret);
+const tapi = new ApiClient({ authProvider });
+
+const api = require("../api/index");
+
+const getLiveChannel = () => {
+    return new Promise((resolve, reject) => {
+        global.client.discord.guilds.fetch(config.modsquad_discord).then(guild => {
+            guild.channels.fetch(config.live_channel).then(channel => {
+                resolve(channel);
+            }, reject);
+        }, reject);
+    });
+}
+
+module.exports = () => {
+    con.query("select distinct tu.id from identity__moderator as im join twitch__user as tu on tu.identity_id = im.modfor_id where im.active = true;", async (err, res) => {
+        if (err) {
+            console.error(err); return;
+        }
+
+        let channel = await getLiveChannel();
+
+        let userList = [];
+
+        let streams = [];
+
+        const getStreams = async () => {
+            const retrievedStreams = await tapi.helix.streams.getStreams({
+                limit: 100,
+                userId: userList,
+            });
+
+            streams = [
+                ...streams,
+                ...retrievedStreams.data,
+            ];
+
+            userList = [];
+        }
+
+        for (let i = 0; i < res.length; i++) {
+            userList = [
+                ...userList,
+                res[i].id
+            ];
+
+            if (userList.length === 100) await getStreams();
+        }
+
+        if (userList.length > 0) await getStreams();
+
+        con.query("select identity_id from live where end_time is null;", async (errl, resl) => {
+            if (errl) {
+                console.error(errl);
+                return;
+            }
+
+            let activeStreams = [];
+
+            resl.forEach(liveChannel => {
+                activeStreams = [
+                    ...activeStreams,
+                    liveChannel.identity_id,
+                ]
+            });
+        
+            for (let si = 0; si < streams.length; si++) {
+                let stream = streams[si];
+                let user = await api.Twitch.getUserById(stream.userId);
+    
+                if (user.identity?.id) {
+                    let identity = await api.getFullIdentity(user.identity.id);
+    
+                    if (!activeStreams.includes(identity.id)) {
+                        con.query("insert into live (identity_id) values (?);", [identity.id], async err => {
+                            if (err) console.error(err);
+
+                            const embed = new MessageEmbed()
+                                .setTitle(`🔴 ${user.display_name} is now live!`)
+                                .setColor(0x7d3bdc)
+                                .setURL("https://twitch.tv/" + user.display_name.toLowerCase())
+                                .setThumbnail(stream.getThumbnailUrl(256, 144))
+                                .addField("Title", stream.title, true)
+                                .addField("Game", stream.gameName, true)
+                                .addField("Viewer Count", ""+stream.viewers, true)
+                                .setTimestamp(stream.startDate)
+                                .setFooter(`${user.display_name} : Live 🔴`, user.profile_image_url);
+                
+                            channel.send({content: ' ', embeds: [embed]});
+                        });
+                    } else {
+                        activeStreams = activeStreams.filter(x => x != identity.id);
+                    }
+                }
+            }
+
+            activeStreams.forEach(async activeStream => {
+                let identity = await api.getFullIdentity(activeStream);
+
+                con.query("update live set end_time = now() where identity_id = ?;", [identity.id], err => {
+                    if (err) {
+                        console.error();
+                        return;
+                    }
+
+                    let user = identity.twitchAccounts[0];
+
+                    const embed = new MessageEmbed()
+                        .setTitle(`${user.display_name} has gone offline!`)
+                        .setColor(0x451b7f)
+                        .setURL("https://twitch.tv/" + user.display_name.toLowerCase())
+                        .setTimestamp(new Date())
+                        .setFooter(`${user.display_name} : Offline`, user.profile_image_url);
+        
+                    channel.send({content: ' ', embeds: [embed]});
+                });
+            });
+        });
+    });
+};
