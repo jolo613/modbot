a2427356408a53f2772d430e567bc0842b02b85b
Twijn <twijn@twijn.net>
2022-01-15 18:32:42 +0000

    Remove old, unused features/files.

--- app.js
+++ app.js
@@ -12,23 +12,17 @@ require("./twitch/twitch");
 // Load Express Backend
 require("./backend/express");
 
-// Load websocket
-require("./websocket/websocket");
-
 // Load global intervals
 const updateUsers = require("./interval/updateUsers");
-const fetchModerators = require("./interval/fetchModerators");
 const updateTwitchUsernames = require("./interval/updateTwitchUsernames");
 const updateDiscordUsernames = require("./interval/updateDiscordUsernames");
 const updateLiveChannels = require("./interval/updateLiveChannels");
 
 updateUsers();
-fetchModerators();
 updateTwitchUsernames();
 updateDiscordUsernames();
 
 setInterval(updateUsers, 10000);
-setInterval(fetchModerators, 30000);
 setInterval(updateTwitchUsernames, 60000);
 setInterval(updateDiscordUsernames, 60000);
 setInterval(updateLiveChannels, 15000);
