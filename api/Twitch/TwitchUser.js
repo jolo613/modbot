343817bd756b0946a2fd826a74a28c345709ba7f
Tyler Twining <twijn@twijn.net>
2022-01-15 17:14:09 -0500

    Testing

--- api/Twitch/TwitchUser.js
+++ api/Twitch/TwitchUser.js
@@ -332,7 +332,6 @@ class TwitchUser extends User {
      */
     post() {
         return new Promise(async (resolve, reject) => {
-            console.log(this.identity);
             con.query("insert into twitch__user (id, display_name, identity_id, email, profile_image_url, offline_image_url, description, view_count, follower_count, affiliation) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?) on duplicate key update display_name = ?, identity_id = ?, email = ?, profile_image_url = ?, offline_image_url = ?, description = ?, view_count = ?, follower_count = ?, affiliation = ?;", [
                 this.id,
                 this.display_name,
