00f8cee7f5e273c3f2ea5750945020a94c88b43f
Tyler Twining <twijn@twijn.net>
2022-01-21 14:00:14 -0500

    change types

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -7,10 +7,10 @@ router.get("/", (req, res) => {
     api.Twitch.Chat.getLogs(
         req.query.channel,
         req.query.user,
-        req.query.time_start,
-        req.query.time_end,
-        req.query.limit,
-        req.query.offset
+        req.query.time_start ? new Date(req.query.time_start) : undefined,
+        req.query.time_end ? new Date(req.query.time_end) : undefined,
+        req.query.limit ? Number(req.query.limit) : undefined,
+        req.query.offset ? Number(req.query.offset) : undefined
     ).then(logs => {
         res.json({success: true, data: logs});
     }, err => {
