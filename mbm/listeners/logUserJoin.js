a45290d7a7204e7e4b7cbec8adaad50c702fdeba
Tyler Twining <twijn@twijn.net>
2022-01-24 10:01:52 -0500

    Start logging discord user joins

--- mbm/listeners/logUserJoin.js
+++ mbm/listeners/logUserJoin.js
@@ -5,7 +5,15 @@ const listener = {
     eventName: 'guildMemberAdd',
     eventType: 'on',
     listener (member) {
-        // con.query("insert into discord__user (id, name, discriminator, avatar) values (?, ?, ?, ?) on duplicate key update name = ?, discriminator = ?, avatar = ?;");
+        con.query("insert into discord__user (id, name, discriminator, avatar) values (?, ?, ?, ?) on duplicate key update name = ?, discriminator = ?, avatar = ?;", [
+            member.id,
+            member.displayName,
+            member.discriminator,
+            member.avatar,
+            member.displayName,
+            member.discriminator,
+            member.avatar,
+        ]);
     }
 };
 
