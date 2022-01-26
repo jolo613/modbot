b36dba01321e5f525d267b297ddc596146b88108
Tyler Twining <twijn@twijn.net>
2022-01-26 11:35:12 -0500

    Implement resources command for sexual assult, suicide, etc

---
+++ mbm/commands/resources.js
@@ -0,0 +1,28 @@
+const {MessageEmbed} = require("discord.js");
+const resources = require("./resources.json");
+
+const command = {
+    data: {
+        name: 'resources'
+        , description: 'View resources for suicide, sexual assault, domestic violence, and more.'
+    },
+    execute(interaction) {
+        let embed = new MessageEmbed()
+            .setTitle("National Hotlines")
+            .setURL("https://victimconnect.org/resources/national-hotlines/")
+            .setColor(0x2170d1);
+
+        resources.forEach(resourceGroup => {
+            let links = "";
+            resourceGroup.links.forEach(link => {
+                if (links !== "") links += "\n";
+                links += `[${link.name}](${link.url}) - ${link.hotline}`;
+            });
+            embed.addField(resourceGroup.name, links, false);
+        });
+
+        interaction.reply({content: ' ', embeds: [embed], ephemeral: true})
+    }
+};
+
+module.exports = command;
