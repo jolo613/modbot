85d5234927f0c0ac3f783e734056b72f78c05f59
Tyler Twining <twijn@twijn.net>
2022-02-06 17:20:51 -0500

    Retrieve messages and members when ModBot is started

--- mbm/listeners/loadGuildCommands.js
+++ mbm/listeners/loadGuildCommands.js
@@ -7,9 +7,27 @@ const listener = {
     name: 'loadGuildCommands',
     eventName: 'ready',
     eventType: 'once',
-    listener () {
-        client.guilds.cache.forEach(guild => {
-            Discord.getGuild(guild.id).then(() => {}).catch(err => {
+    async listener () {
+        await client.guilds.fetch();
+        client.guilds.cache.forEach(async guild => {
+            const members = await guild.members.fetch();
+            console.log(`Fetched members for ${guild.name}: ${members.size} members`)
+
+            guild.channels.fetch().then(channels => {
+                channels.forEach(channel => {
+                    if (channel.type === "GUILD_TEXT") {
+                        channel.messages.fetch().then(() => {}, console.error); // By default will just fetch 50 messages.
+                    }
+                });
+            }, console.error);
+
+            Discord.getGuild(guild.id).then(dGuild => {
+                guild.members.cache.forEach(member => {
+                    Discord.getUserById(member.id, false, true).then(dUser => {
+                        dGuild.addUser(dUser).then(() => {}, console.error);
+                    }, console.error);
+                });
+            }).catch(err => {
                 guild.commands.create(registerCommand.data).then(command => {
                     command.permissions.add({guild: guild.id, command: command.id, permissions: [{
                         id: guild.ownerId,
