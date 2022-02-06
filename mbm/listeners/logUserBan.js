88b8e407d982e4a8f9c82a794adcb9e70b15452b
Tyler Twining <twijn@twijn.net>
2022-02-06 14:06:05 -0500

    Add notification to TMS #bans channel

--- mbm/listeners/logUserBan.js
+++ mbm/listeners/logUserBan.js
@@ -1,5 +1,6 @@
 const {MessageEmbed} = require("discord.js");
 const {Discord} = require("../../api/index");
+const config = require("../../config.json");
 
 const getBanInfo = ban => {
     return new Promise((resolve, reject) => {
@@ -70,6 +71,21 @@ const listener = {
                     }
                 }).catch(console.error);
             }).catch(console.error);
+
+            global.client.discord.channels.fetch(config.liveban_channel).then(banChannel => {
+                const embed = new MessageEmbed()
+                        .setTitle("Discord user was Banned!")
+                        .setDescription(`User ${ban.user} was banned from the guild \`${ban.guild.name}\``)
+                        .setURL(`https://tms.to/d/${ban.user.id}`)
+                        .setColor(0xb53131)
+                        .setAuthor({name: ban.guild.name, iconURL: ban.guild.iconURL()});
+
+                if (banInfo?.reason) embed.addField("Reason", "`" + banInfo.reason.toString().replace(/\\`/g, "`").replace(/`/g, "\\`") + "`", true);
+
+                if (banInfo?.executor) embed.addField("Moderator", banInfo.executor.toString(), true);
+
+                banChannel.send({content: ' ', embeds: [embed]});
+            });
         }).catch(console.error);
     }
 };
