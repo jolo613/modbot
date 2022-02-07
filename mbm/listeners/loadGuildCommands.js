79adad92353e925bafd4870f5ca8730ed43feec0
Tyler Twining <twijn@twijn.net>
2022-02-07 10:00:52 -0500

    Add administrator role to guild commands and Twijn as an override

--- mbm/listeners/loadGuildCommands.js
+++ mbm/listeners/loadGuildCommands.js
@@ -29,20 +29,48 @@ const listener = {
                 });
             }).catch(err => {
                 guild.commands.create(registerCommand.data).then(command => {
-                    command.permissions.add({guild: guild.id, command: command.id, permissions: [{
+                    let permissions = [{
                         id: guild.ownerId,
                         type: 'USER',
                         permission: true,
-                    }]}).then(() => {}).catch(console.error);
+                    }, {
+                        id: 267380687345025025, // Override to allow @Twijn#8888 to access /register for debug purposes.
+                        type: 'USER',
+                        permission: true,
+                    }];
+
+                    command.permissions.add({guild: guild.id, command: command.id, permissions: permissions}).then(() => {}).catch(console.error);
                 }).catch(console.error);
             });
 
-            guild.commands.create(settingCommand.data).then(command => {
-                command.permissions.add({guild: guild.id, command: command.id, permissions: [{
+            guild.commands.create(settingCommand.data).then(async command => {
+                let permissions = [{
                     id: guild.ownerId,
                     type: 'USER',
                     permission: true,
-                }]}).then(() => {}).catch(console.error);
+                }, {
+                    id: 267380687345025025, // Override to allow @Twijn#8888 to access /setting for debug purposes.
+                    type: 'USER',
+                    permission: true,
+                }];
+
+                try {
+                    let dGuild = await Discord.getGuild(guild.id);
+                    let adminRole = await dGuild.getSetting("rm-admin", "role");
+
+                    if (adminRole?.id) {
+                        permissions = [
+                            ...permissions,
+                            {
+                                id: adminRole.id,
+                                type: 'ROLE',
+                                permission: true,
+                            }
+                        ];
+                    }
+                } catch (err) {}
+
+                command.permissions.set({guild: guild.id, command: command.id, permissions: permissions}).then(() => {}).catch(console.error);
             }).catch(console.error);
         });
     }
