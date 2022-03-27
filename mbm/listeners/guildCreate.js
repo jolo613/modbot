1eb8479ce7a1a1ba766d553df2c48fe539157189
Tyler Twining <twijn@twijn.net>
2022-03-27 00:13:34 -0400

    Add message when MBM is added to a guild

--- mbm/listeners/guildCreate.js
+++ mbm/listeners/guildCreate.js
@@ -1,5 +1,6 @@
 const {Discord} = require("../../api/index");
 const registerCommand = require("../commands/register");
+const {MessageEmbed} = require("discord.js");
 
 const listener = {
     name: 'guildCreate',
@@ -25,6 +26,19 @@ const listener = {
 
             dGuild.addCommands(guild);
         }).catch(err => {
+            const msg = {content: ' ', embeds: [
+                new MessageEmbed()
+                    .setTitle("Configure MBM for your server")
+                    .setDescription("Hello, <@" + guild.ownerId + "> .\n\nPlease finish setting up MBM on your server using the following commands.\n\n`/register` - Register your Discord guild. The command should include the discord user that the guild represents and the twitch username of that user.\n`/setting` - set local settings on how MBM should handle your guild")
+                    .setColor(0x9403fc)
+                    .setFooter({text: "Enter the above commands in any text channel in your guild.", iconURL: "https://twitchmodsquad.com/assets/images/logo.webp"})
+            ]};
+            guild.members.fetch(guild.ownerId).then(owner => {
+                owner.send(msg).then(() => {}, err => {
+                    // TODO: Add backup plan.
+                });
+            });
+
             guild.commands.create(registerCommand.data).then(command => {
                 command.permissions.set({guild: guild.id, command: command.id, permissions: [{
                     id: guild.ownerId,
