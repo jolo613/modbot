dbdbb24de315afd78bb7b289b4deadb6cbb7952c
Tyler Twining <twijn@twijn.net>
2022-04-08 17:44:02 -0400

    Start implementation of shortlink management

--- backend/routes/shortlink.js
+++ backend/routes/shortlink.js
@@ -7,8 +7,31 @@ const router = Router();
 router.use(bodyParser.urlencoded({extended: true}));
 router.use(bodyParser.json());
 
+const parseShortlink = shortlink => {
+    return new Promise(async (resolve, reject) => {
+        if (shortlink.name !== null) {
+            con.query("select * from shortlink__group where shortlink_id = ?;", [shortlink.id], (err, res) => {
+                if (err) {reject(err);return;}
+
+
+                resolve(shortlink);
+            });
+        } else
+            resolve(shortlink);
+    });
+}
+
 router.get("/", (req, res) => {
-    res.json({success: false, error: "GET is not allowed"});
+    con.query("select * from shortlink where created_id = ?;", [req.session.identity.id], (err, response) => {
+        if (err) {
+            res.json({success: false, error: err});
+            return;
+        }
+
+        response.map()
+
+        res.json({success: true, data: response});
+    });
 });
 
 router.get("/:shortlink", (req, res) => {
