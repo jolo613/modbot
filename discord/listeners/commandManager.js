626701f4b7b3dd8d5982a11438d1fb49dfb55f7b
Tyler Twining <twijn@twijn.net>
2022-04-27 19:24:49 -0400

    Add success & error functions to all commands via injection in command manager

--- discord/listeners/commandManager.js
+++ discord/listeners/commandManager.js
@@ -1,3 +1,4 @@
+const {MessageEmbed} = require("discord.js");
 const client = global.client.discord;
 
 const listener = {
@@ -10,6 +11,28 @@ const listener = {
         if (!client.commands.has(interaction.commandName)) return;
     
         let cmd = client.commands.get(interaction.commandName);
+
+        const success = message => {
+            const embed = new MessageEmbed()
+                .setTitle("Success!")
+                .setDescription(message)
+                .setColor(0x772ce8);
+
+            interaction.reply({content: ' ', embeds: [embed], ephemeral: true})
+        }
+
+        const error = message => {
+            const embed = new MessageEmbed()
+                .setTitle("Error!")
+                .setDescription(message)
+                .setColor(0xe83b3b);
+
+            interaction.reply({content: ' ', embeds: [embed], ephemeral: true})
+        }
+
+        interaction.success = success;
+        interaction.error = error;
+
         try {
             cmd.execute(interaction);
         } catch (error) {
