c0f23f8af5303ca7fa613f1bbdf5adae70e1462f
Tyler Twining <twijn@twijn.net>
2022-05-14 15:33:08 -0400

    Implement file manager

---
+++ discord/listeners/archiveFileManagerButtons.js
@@ -0,0 +1,53 @@
+const {cache} = require("../commands/archive");
+const {Modal, TextInputComponent, showModal} = require("discord-modals");
+const api = require("../../api/index");
+
+const fs = require("fs");
+
+const listener = {
+    name: 'archiveFileManagerButtons',
+    eventName: 'interactionCreate',
+    eventType: 'on',
+    listener (interaction) {
+        if (interaction.isButton()) {
+            if (interaction.component.customId === "set-label") {
+                const modal = new Modal()
+                    .setCustomId("set-label-" + interaction.message.id)
+                    .setTitle("Set File Label")
+                    .addComponents(
+                        new TextInputComponent()
+                            .setCustomId("label")
+                            .setLabel("Label")
+                            .setStyle("SHORT")
+                            .setMinLength(3)
+                            .setMaxLength(64)
+                            .setRequired(true)
+                    );
+
+                showModal(modal, {
+                    client: global.client.discord,
+                    interaction: interaction,
+                });
+            } else if (interaction.component.customId === "remove-file") {
+                api.Discord.getUserById(interaction.member.id).then(user => {
+                    let file = cache[user.identity.id].files.find(x => x.message.id === interaction.message.id);
+
+                    if (file && file.hasOwnProperty("local_path")) {
+                        fs.unlink(file.local_path, err => {
+                            if (err) console.error(err);
+                        });
+                    }
+
+                    cache[user.identity.id].files = cache[user.identity.id].files.filter(x => x.message.id !== interaction.message.id);
+                    interaction.message.delete();
+                    console.log(cache[user.identity.id]);
+                }, err => {
+                    console.error(err);
+                    interaction.reply("You're not properly linked to TMS");
+                });
+            }
+        }
+    }
+};
+
+module.exports = listener;
